4cfe5c833b2a865cadeb3c52d93123374bff1d44
AdhyanBaluja <adhyanbaluja@gmail.com>
2025-04-11 00:32:05 +0530

    new colors signup form influencer

--- client/src/components/InfluencerSignUpForm.jsx
+++ client/src/components/InfluencerSignUpForm.jsx
@@ -326,6 +326,13 @@ function InfluencerSignUpForm() {
   };
   
 
+  // Form step variants for animations
+  const formVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+    exit: { opacity: 0, x: 100, transition: { duration: 0.3 } }
+  };
+
   return (
     
     <div className="influencer-page-container">
@@ -1100,6 +1107,7 @@ function InfluencerSignUpForm() {
                         type="submit" 
                         className="primary-btn submit-btn-1"
                         disabled={isSubmitting}
+
                       >
                         {isSubmitting ? (
                           <div className="loader-container">
